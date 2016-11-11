2c74ce255eb294f55f5e0ab29f7e1493cb8d60b7
Johannes Neumeier <hello@johannesneumeier.com>
2016-11-11 10:57:06 +0200

    toggle a class on FeatureLister container for when the font has no features

--- examples/simple/main.js
+++ examples/simple/main.js
@@ -10,6 +10,7 @@ define([
   , 'specimenTools/widgets/CurrentWebFont'
   , 'specimenTools/widgets/TypeTester'
   , 'specimenTools/widgets/FontLister'
+  , 'specimenTools/widgets/FeatureLister'
 ], function(
     loadFonts
   , initDocumentWidgets
@@ -22,6 +23,7 @@ define([
   , CurrentWebFont
   , TypeTester
   , FontLister
+    , FeatureLister
 ) {
     "use strict";
 
@@ -46,6 +48,7 @@ define([
             // All Constructors are given [dom-container, pubsub] as the first two arguments.
             //['family-chooser', FamilyChooser, fontsData]
             ['font-lister', FontLister, fontsData]
+          , ['feature-lister', FeatureLister, fontsData]
           , ['glyph-table', GlyphTables]
           , ['font-data', GenericFontData, fontsData]
           , ['current-font', CurrentWebFont, webFontProvider]
